b2ead2f140bfff95a16c0069cb5bf34646f6e27b
Boosh7400 <daniil.bush74@proton.me>
2023-01-11 20:56:05 +0200

    Roll and Enemy added

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 const FRICTION = 600
 const ACCELERATION = 600
 const MAX_SPEED = 60
+const ROLL_SPEED = 85
 
 enum {
 	MOVE,
@@ -12,13 +13,16 @@ enum {
 
 var state = MOVE
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.DOWN
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var swordHitbox = $HitboxPivot/SwordHitbox
 
 func _ready():
 	animationTree.active = true
+	swordHitbox.knockback_vector = roll_vector
 
 func _process(delta):
 	match state:
@@ -27,7 +31,7 @@ func _process(delta):
 		ATTACK:
 			attack_state(delta)
 		ROLL:
-			pass
+			roll_state(delta)
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO
@@ -36,26 +40,42 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
+		swordHitbox.knockback_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	velocity = move_and_slide(velocity)
+	move()
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
+	
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
 
 func attack_state(delta):
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
 	
 func roll_state(delta):
-	pass
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move()
+
+func move():
+	velocity = move_and_slide(velocity)
+
+
+func roll_animation_finished():
+	velocity = Vector2.ZERO
+	state = MOVE
 
 func attack_animation_finished():
 	state = MOVE
