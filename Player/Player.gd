088197bd5b125734706f95eb386ddac36af87d1e
Boosh7400 <daniil.bush74@proton.me>
2023-01-15 21:28:12 +0200

    Enemy collisions and wander state

--- Player/Player.gd
+++ Player/Player.gd
@@ -23,6 +23,7 @@ onready var swordHitbox = $HitboxPivot/SwordHitbox
 onready var hurtbox = $Hurtbox
 
 func _ready():
+	randomize()
 	stats.connect("no_health", self, "queue_free")
 	animationTree.active = true
 	swordHitbox.knockback_vector = roll_vector
@@ -37,6 +38,7 @@ func _process(delta):
 			roll_state(delta)
 
 func move_state(delta):
+	
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down")-Input.get_action_strength("ui_up")
